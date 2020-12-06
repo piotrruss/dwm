0480782cda74b66f7794e139a827823e8e0b49a7
Piotr Russ <mail@pruss.it>
2020-12-06 16:06:22 +0100

    add new bottom layout

--- config.def.h
+++ config.def.h
@@ -46,9 +46,10 @@ static const int resizehints = 0;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
   /* symbol     arrange function */
-  { "",  tile },    /* first entry is default */
+  { "",  bstack },    /* first entry is default */
   { "[F]",  NULL },    /* no layout function means floating behavior */
   { "[M]",  monocle },
+  { "[T]",  tile },
 };
 
 /* key definitions */
@@ -84,6 +85,7 @@ static Key keys[] = {
   { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
   { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
   { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+  { MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[3]} },
   { MODKEY,                       XK_space,  setlayout,      {0} },
   { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
   { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
@@ -102,7 +104,7 @@ static Key keys[] = {
   TAGKEYS(                        XK_8,                      7)
   TAGKEYS(                        XK_9,                      8)
   { MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-  { MODKEY|ShiftMask,             XK_Escape, spawn,          SHCMD("sudo systemtctl suspend") },
+  { MODKEY|ShiftMask,             XK_Escape, spawn,       SHCMD("lockAndSleep") },
   /* custom bindings */
   { MODKEY,              XK_w,   spawn,  SHCMD("firefox") },
   { MODKEY|ShiftMask,    XK_w,   spawn,  SHCMD("google-chrome-stable") },
