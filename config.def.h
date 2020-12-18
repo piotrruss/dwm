e01998447c29203fde4a03543a5f6a546114ff5b
Piotr Russ <mail@pruss.it>
2020-12-18 23:20:01 +0100

    actual fullscreen + bindings

--- config.def.h
+++ config.def.h
@@ -85,11 +85,12 @@ static Key keys[] = {
   { MODKEY,                       XK_q,      killclient,     {0} },
   { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
   { MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[1]} },
-  { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[2]} },
+  { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
   { MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[3]} },
   { MODKEY,                       XK_space,  setlayout,      {0} },
   { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
   { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+  { MODKEY,                       XK_f,      togglefullscr,  {0} },
   { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
   { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
   { MODKEY,                       XK_period, focusmon,       {.i = +1 } },
@@ -127,7 +128,7 @@ static Key keys[] = {
   /* { MODKEY|ShiftMask,    XK_x,   spawn,  SHCMD("") }, */
   { MODKEY,              XK_c,   spawn,  SHCMD(TERM "calcurse") },
   { MODKEY|ShiftMask,    XK_c,   spawn,  SHCMD("~/Scripts/camtoggle") },
-  { MODKEY,              XK_m,   spawn,  SHCMD(TERM "ncmpcpp") },
+  { MODKEY,              XK_u,   spawn,  SHCMD(TERM "ncmpcpp") },
   { MODKEY|ShiftMask,    XK_m,   spawn,  SHCMD(TERM "pulsemixer") },
   /* { MODKEY|ShiftMask,    XK_x,   spawn,  SHCMD(TERM "") }, */
 
